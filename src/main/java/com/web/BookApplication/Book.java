37ab61c8772e395fc90a891aab6511cb18678e63
logicstark <logicalstark1105@gmail.com>
2020-03-05 17:08:11 +0900

    first commit

---
+++ src/main/java/com/web/BookApplication/Book.java
@@ -0,0 +1,19 @@
+package com.web.BookApplication;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+}
